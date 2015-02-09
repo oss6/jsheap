854f9f8959846581438d995e18b62af9d4cb239f
oss6 <ossedb@gmail.com>
2015-02-09 12:01:12 +0000

    Writing tests

--- test/test.js
+++ test/test.js
@@ -6,8 +6,14 @@ suite('HeapTree', function () {
 	var heap = new ht();
 	
 	test('isEmpty should return true when the array length is 0', function () {
-		
 		assert.equal(true, heap.isEmpty());
+	});
+	
+	test('isEmpty should return false when the array length is > 0', function () {
+		heap = new ht({
+			src: [1, 2, 3]
+		});
+		assert.equal(false, heap.isEmpty());
 	});
 	
 });
