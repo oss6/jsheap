cdf2f85ff99feb2313629728208df58ea7439222
oss6 <ossedb@gmail.com>
2015-03-18 20:11:00 +0000

    Added parent definition

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var HeapTree = function (arr, comparator) {
     var _arr = arr;
     var _cmp = comparator;
     
-    var bubbleUp = function (i) {
-    
-    }
-    
     this.root = function () {
     	return this.isEmpty() ? undefined : _arr[0];
     };
@@ -18,13 +14,26 @@ var HeapTree = function (arr, comparator) {
     	return _arr.length === 0;
     };
     
+    this.parent = function (i) {
+    	
+    }
+    	
     this.heapify = function (arr) {
 	
     };
-
+	
+	this.bubbleUp = function (i) {
+    	if (i === 0) // is root
+    		return
+    	else if (heap[i] > heap[this.parent(i)]) {
+    		// swap and
+    		this.bubbleUp(parent(i));
+    	}
+    }
+	
     this.insert = function (k) {
 		_arr.push(k);
-		bubbleUp(_arr.length - 1);
+		this.bubbleUp(_arr.length - 1);
     };
     
     this.del = function (k) {
