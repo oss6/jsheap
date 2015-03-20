8e8d572ae6b78fb0c4ac5ad310bc0e218beeed84
oss6 <ossedb@gmail.com>
2015-03-20 22:13:13 +0000

    More fn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var HeapTree = function (arr, comparator) {
     var _arr = arr;
     
+    this.root = function () {
+    	return this.isEmpty() ? undefined : _arr[0];
+    };
+	
+	this.lastLeaf = function () {
+		return this.isEmpty() ? undefined : _arr[_arr.length - 1];
+	};
+	
+    this.isEmpty = function () {
+    	return _arr.length === 0;
+    };
+    
     this.heapify = function (arr) {
 	
     };
@@ -11,14 +23,6 @@ var HeapTree = function (arr, comparator) {
     
     this.del = function (k) {
     
-    };
-    
-    this.root = function () {
-    	return this.isEmpty() ? undefined : _arr[0];
-    };
-
-    this.isEmpty = function () {
-    	return _arr.length === 0;
     };
 }
 
