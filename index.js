d389e503321bf60c253f25bdcd0c3b7f9c7b099a
oss6 <ossedb@gmail.com>
2015-03-21 02:02:05 +0000

    Adding structure to heap lb

---
+++ index.js
@@ -0,0 +1,15 @@
+var HeapTree = function (arr) {
+    var _arr = arr;
+    
+    this.heapify = function (arr) {
+
+    };
+
+    this.insert = function (k) {
+
+    };
+
+
+}
+
+module.exports = HeapTree;
