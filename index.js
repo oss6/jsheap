46081aa77f41b2c31aa5e3dab13911f4ebdfdb37
oss6 <ossedb@gmail.com>
2015-03-22 22:08:33 +0000

    Added more fn

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-var HeapTree = function (arr) {
+var HeapTree = function (arr, comparator) {
     var _arr = arr;
     
     this.heapify = function (arr) {
-
+	
     };
 
     this.insert = function (k) {
 
     };
+    
+    this.del = function (k) {
+    
+    };
+    
+    this.root = function () {
+    	return this.isEmpty() ? undefined : _arr[0];
+    };
 
-
+    this.isEmpty = function () {
+    	return _arr.length === 0;
+    };
 }
 
 module.exports = HeapTree;
